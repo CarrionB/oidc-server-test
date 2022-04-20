743b714193e8a584300f4fd2cddac8da3d833ee9
CarrionB <ssj4_bryan@yahoo.es>
2022-04-20 13:31:38 -0500

    feat: :sparkles: testing auth code gran type

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,36 @@
 import 'dotenv/config'
 import express from "express";
 import { Provider } from 'oidc-provider';
+const Account = require('./Accounts');
 const {PORT} = process.env
 const configuration = {
   // ... see /docs for available configuration
   clients: [{
     client_id: 'test_implicit_app',
     client_secret: 'bar',
-    redirect_uris: ['https://f7d1-2800-bf0-2c3-b62-f456-c771-1d51-99e2.ngrok.io/oidc'],
+    grant_types: ['refresh_token', 'authorization_code', 'client_credentials', 'password'],
+    redirect_uris: ['https://openidconnect.net/callback'],
     // ... other client properties
   }],
   claims: {
+    address: ['address'],
     email: ['email', 'email_verified'],
     phone: ['phone_number', 'phone_number_verified'],
-    profile: ['birthdate', 'family_name', 'gender', 'given_name', 'locale', 'middle_name', 'name', 'nickname', 'picture', 'preferred_username', 'profile', 'updated_at', 'website', 'zoneinfo']
-  }
+    profile: ['birthdate', 'family_name', 'gender', 'given_name', 'locale', 'middle_name', 'name',
+      'nickname', 'picture', 'preferred_username', 'profile', 'updated_at', 'website', 'zoneinfo'],
+  },
+  cookies: {
+    keys: ['some secret key', 'and also the old rotated away some time ago', 'and one more'],
+  },
+  findAccount: Account.findAccount,
 };
 
 const oidc = new Provider(`http://localhost:${PORT}`, configuration);
 
+const app = express()
+
+app.use(oidc.callback)
+
 oidc.listen(PORT, () => {
   console.log(`oidc-provider listening on port ${PORT}, check http://localhost:${PORT}/.well-known/openid-configuration`);
 });
-
-const app = express()
-
-app.use('/oidc', oidc.callback)
-
